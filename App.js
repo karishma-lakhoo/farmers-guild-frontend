6dbaaa2ed2e768670ed70e386d6d211f42242ac4
Pratham1313 <82656237+BLACKDEATH1313@users.noreply.github.com>
2023-03-23 17:43:45 +0200

    Removed the default header

--- App.js
+++ App.js
@@ -7,19 +7,23 @@ import routes from "./src/config/routes";
 
 const Stack = createStackNavigator();
 
+
 export default function App() {
   return (
-      <NavigationContainer>
-          <Stack.Navigator initialRouteName={'Home'}>
-              {routes.map((r,i) => (
-                  <Stack.Screen key={i} name={r.name}>
-                      {(props) => <r.component nameProp = {r.name} {...props}/>}
-                  </Stack.Screen>
-              ))}
-
-          </Stack.Navigator>
-      </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={'Home'}>
+        {routes.map((r, i) => (
+          <Stack.Screen
+            key={i}
+            name={r.name}
+            component={r.component}
+            options={{ headerShown: false }}
+          />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+
 
