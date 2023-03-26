e70db6afc3023ba86a4737f95e1a07ff0c2e54ba
Suvarn Pillay <105659331+SuvarnPillayWits@users.noreply.github.com>
2023-03-26 15:13:37 +0200

    changed to home for testing purposes.

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'AddGarden'}>
+      <Stack.Navigator initialRouteName={'Home'}>
         {routes.map((r, i) => (
           <Stack.Screen
             key={i}
