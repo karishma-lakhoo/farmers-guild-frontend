825a70e4190206f8b349fc0c88ad759f8d27dc70
karishma <karishma@lakhoo.net>
2023-04-21 21:55:05 +0200

    test code cov

--- src/components/test-component.js
+++ src/components/test-component.js
@@ -1,3 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';const Hello = () => <Text>Hello, world!</Text>;
+import { Text } from 'react-native';
+const Hello = () => <Text>Hello, world!</Text>;
     export default Hello;
