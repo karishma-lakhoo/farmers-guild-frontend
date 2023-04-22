7f3c1aee5e819c669f4a9c3056fa2555410d60e1
Shailyn-Ramsamy <shailyn.ramsamy@gmail.com>
2023-04-23 00:44:21 +0200

    Analytics filters WIP 2

--- src/consts/foods.js
+++ src/consts/foods.js
@@ -1,54 +1,305 @@
-const foods = [
+export const foods = [
     {
-        id: '1',
-        name: 'Supertype',
-        options:[
-            {
-                id: "1",
-                name: "Fruit"
-            },
-            {
-                id: "2",
-                name: "Vegetables"
-            },
-            {
-                id: "3",
-                name: "Herbs"
-            },
-            {
-                id: "4",
-                name: "Flower"
-            }
-            ]
+        key: 'SUP',
+        value: 'Supertype',
     },
     {
-        id: '2',
-        name: 'Type',
-        subtype: 'Fruit',
-        options:[
-            {
-                id: "1",
-                name: "blahhh"
-            },
-            {
-                id: "2",
-                name: "bloop"
-            },
-            {
-                id: "3",
-                name: "Herbadkws"
-            },
-            {
-                id: "4",
-                name: "Fladwodwkower"
-            }
-        ]
+        key: 'TYP',
+        value: 'Type',
     },
     {
-        id: '3',
-        name: 'Subtype',
-        subtype: 'Fruit',
+        key: 'SUB',
+        value: 'Subtype'
     }
 ];
 
-export default foods;
+export const foodSubCategories = {
+    'SUP': [
+        {
+            key: '1',
+            value: 'Fruits'
+        },
+        {
+            key: '2',
+            value: 'Vegetables'
+        },
+        {
+            key: '3',
+            value: 'Herbs'
+        },
+        {
+            key: '4',
+            value: 'Flowers'
+        }],
+    'TYP': [
+        {
+            key: '5',
+            value: 'Nut'
+        },
+        {
+            key: '6',
+            value: 'Pome Fruit'
+        },
+        {
+            key: '7',
+            value: 'Flower'
+        },
+        {
+            key: '8',
+            value: 'Nightshade'
+        },
+        {
+            key: '9',
+            value: 'Soft Herb'
+        },
+        {
+            key: '10',
+            value: 'Legume'
+        },
+        {
+            key: '11',
+            value: 'Root'
+        },
+        {
+            key: 12,
+            value: 'Berry'
+        },
+        {
+            key: 13,
+            value: 'Cruciferous'
+        },
+        {
+            key: 14,
+            value: 'Cucurbit'
+        },
+        {
+            key: 15,
+            value: 'Stalk'
+        },
+        {
+            key: 16,
+            value: 'Allium'
+        },
+        {
+            key: 17,
+            value: 'Woody Herb'
+        },
+        {
+            key: 18,
+            value: 'False Fruit'
+        },
+        {
+            key: 19,
+            value: 'Vine Fruit'
+        },
+        {
+            key: 20,
+            value: 'Citrus'
+        },
+        {
+            key: 21,
+            value: 'Hard Herb'
+        },
+        {
+            key: 22,
+            value: 'Leaf'
+        },
+        {
+            key: 23,
+            value: 'Stone Fruit'
+        },
+        {
+            key: 24,
+            value: 'Seed'
+        }],
+    'SUB': [
+        {
+            key: 25,
+            value: 'Almond'
+        },
+        {
+            key: 26,
+            value: 'Apple'
+        },
+        {
+            key: 27,
+            value: 'Artichoke'
+        },
+        {
+            key: 28,
+            value: 'Aubergine'
+        },
+        {
+            key: 29,
+            value: 'Basil'
+        },
+        {
+            key: 30,
+            value: 'Bean'
+        },
+        {
+            key: 31,
+            value: 'Beetroot'
+        },
+        {
+            key: 32,
+            value: 'Blackberry'
+        },
+        {
+            key: 33,
+            value: 'Blueberry'
+        },
+        {
+            key: 34,
+            value: 'Broccoli'
+        },
+        {
+            key: 35,
+            value: 'Squash'
+        },
+        {
+            key: 36,
+            value: 'Cabbage'
+        },
+        {
+            key: 37,
+            value: 'Carrot'
+        },
+        {
+            key: 38,
+            value: 'Cauliflower'
+        },
+        {
+            key: 39,
+            value: 'Kale'
+        },
+        {
+            key: 40,
+            value: 'Celery'
+        },
+        {
+            key: 41,
+            value: 'Chilli'
+        },
+        {
+            key: 42,
+            value: 'Chive'
+        },
+        {
+            key: 43,
+            value: 'Coriander'
+        },
+        {
+            key: 44,
+            value: 'Cucumber'
+        },
+        {
+            key: 45,
+            value: 'Curry Leaf'
+        },
+        {
+            key: 46,
+            value: 'Edible Flower'
+        },
+        {
+            key: 47,
+            value: 'Fennel'
+        },
+        {
+            key: 48,
+            value: 'Fig'
+        },
+        {
+            key: 49,
+            value: 'Ginger'
+        },
+        {
+            key: 50,
+            value: 'Gooseberry'
+        },
+        {
+            key: 51,
+            value: 'Granadilla'
+        },
+        {
+            key: 52,
+            value: 'Grape'
+        },
+        {
+            key: 53,
+            value: 'Grapefruit'
+        },
+        {
+            key: 54,
+            value: 'Jerusalem Artichoke'
+        },
+        {
+            key: 55,
+            value: 'Kei Apple'
+        },
+        {
+            key: 56,
+            value: 'Leek'
+        },
+        {
+            key: 57,
+            value: 'Lemon'
+        },
+        {
+            key: 58,
+            value: 'Lemon Balm'
+        },
+        {
+            key: 59,
+            value: 'Lemon Verbena'
+        },
+        {
+            key: 60,
+            value: 'Lettuce'
+        },
+        {
+            key: 61,
+            value: 'Lime'
+        },
+        {
+            key: 62,
+            value: 'Marjoram'
+        },
+        {
+            key: 63,
+            value: 'Mint'
+        },
+        {
+            key: 64,
+            value: 'Mustard Leaf'
+        },
+        {
+            key: 65,
+            value: 'Naartjie'
+        },
+        {key: 66,value: 'Nasturtium'},
+        { key: 67, value: 'Onion' },
+        { key: 68, value: 'Orange' },
+        { key: 69, value: 'Oregano' },
+        { key: 70, value: 'Parsley' },
+        { key: 71, value: 'Pea' },
+        { key: 72, value: 'Peach' },
+        { key: 73, value: 'Pepper' },
+        { key: 74, value: 'Plum' },
+        { key: 75, value: 'Pumpkin' },
+        { key: 76, value: 'Radish' },
+        { key: 77, value: 'Rhubarb' },
+        { key: 78, value: 'Rosemary' },
+        { key: 79, value: 'Sage' },
+        { key: 80, value: 'Sorrel' },
+        { key: 81, value: 'Spinach' },
+        { key: 82, value: 'Strawberry' },
+        { key: 83, value: 'Sunflower Seed' },
+        { key: 84, value: 'Sweet Potato' },
+        { key: 85, value: 'Thyme' },
+        { key: 86, value: 'Tomato' },
+        { key: 87, value: 'Turmeric' },
+        { key: 88, value: 'Turnip' },
+        { key: 89, value: 'Zucchini' }
+    ]
+    }
