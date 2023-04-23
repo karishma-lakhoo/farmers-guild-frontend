3d5442d0a6778a1e0eb588299631eda2a0987f7a
karishma <karishma@lakhoo.net>
2023-04-23 19:27:21 +0200

    All works for supertypes and types

---
+++ src/consts/types_pie.js
@@ -0,0 +1,22 @@
+const types_pie = [
+    {"Nut": 0},
+    {"Pome Fruit": 0},
+    {"Flower": 0},
+    {"Nightshade": 0},
+    {"Legume": 0},
+    {"Cruciferous": 0},
+    {"Cucurbit": 0},
+    {"Root": 0},
+    {"Berry": 0},
+    {"Vine Fruit": 0},
+    {"Citrus": 0},
+    {"Allium": 0},
+    {"Leaf": 0},
+    {"Soft Herb": 0},
+    {"Woody Herb": 0},
+    {"Hard Herb": 0},
+    {"False Fruit": 0},
+    {"Stone Fruit": 0},
+    {"Seed": 0}
+]
+export default types_pie;
